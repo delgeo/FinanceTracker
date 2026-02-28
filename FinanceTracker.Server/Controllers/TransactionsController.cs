f34cba04d14167bb769eb6e166fcb64e68535c9b
deligeo <georgedelna10@gmail.com>
2026-02-28 12:17:33 -0500

    Added TransactionsController with GET, POST, and DELETE actions

---
+++ FinanceTracker.Server/Controllers/TransactionsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FinanceTracker.Server.Data;
+using FinanceTracker.Server.Models;
+
+namespace FinanceTracker.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        // The "Waiter" gets a reference to the "Chef" (Database) here
+        public TransactionsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/transactions (Fetch all data)
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Transaction>>> GetTransactions()
+        {
+            return await _context.Transactions.ToListAsync();
+        }
+
+        // POST: api/transactions (Save a new transaction)
+        [HttpPost]
+        public async Task<ActionResult<Transaction>> PostTransaction(Transaction transaction)
+        {
+            _context.Transactions.Add(transaction);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetTransactions), new { id = transaction.Id }, transaction);
+        }
+
+        // DELETE: api/transactions/5 (Remove data)
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTransaction(int id)
+        {
+            var transaction = await _context.Transactions.FindAsync(id);
+            if (transaction == null) return NotFound();
+
+            _context.Transactions.Remove(transaction);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
